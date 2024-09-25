d03028b128f74917f7dbff1c63c3b7d94f7221ce
saurav7356 <148684566+saurav7356@users.noreply.github.com>
2024-09-25 15:37:56 +0530

    Calculatorjavscript

---
+++ test.js
@@ -0,0 +1,22 @@
+// const accountId = 123
+// let accountEmail = "saurav@gamil.com"
+// var accountPassword = "@12345"
+// accountCity = "Patna"
+// // accountId = 2
+// console.log(accountId);
+// console.log([accountEmail,accountPassword]);
+
+// const name = "saurav"
+// const repocount = 50
+
+// console.log(name + repocount + " value");
+
+// console.log(`Hello my name is ${name} and my name repo count is ${repocount}`);
+
+// const gameName = new String('saurav sinha')
+
+let myDAte = new Date()
+console.log(myDAte.toString());
+console.log(myDAte.toDateString());
+console.log(myDAte.toLocaleString());
+let myCreateDate = new Date ("01-04-2024")
